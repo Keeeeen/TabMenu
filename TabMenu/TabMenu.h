aeec59161f593ff5ab51779c4f74a8098076bc04
Keeeeen <c0114164fd@edu.teu.ac.jp>
2016-10-04 11:35:30 +0900

    Initial Commit

---
+++ TabMenu/TabMenu.h
@@ -0,0 +1,19 @@
+//
+//  TabMenu.h
+//  TabMenu
+//
+//  Created by K.Kawakami on 2016/10/04.
+//  Copyright © 2016年 川上健太郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TabMenu.
+FOUNDATION_EXPORT double TabMenuVersionNumber;
+
+//! Project version string for TabMenu.
+FOUNDATION_EXPORT const unsigned char TabMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TabMenu/PublicHeader.h>
+
+
